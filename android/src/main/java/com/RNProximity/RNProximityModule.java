a225dd9683ae0ab1a9a6467d87e2688ae67ff5d0
wmatyjanomtek <w.matyja@nomtek.com>
2022-01-03 11:56:57 +0100

    changed implementation of android

--- android/src/main/java/com/RNProximity/RNProximityModule.java
+++ android/src/main/java/com/RNProximity/RNProximityModule.java
@@ -1,42 +1,29 @@
 
 package com.RNProximity;
-
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-import java.util.HashMap;
-import java.util.Map;
+import static android.content.Context.POWER_SERVICE;
 
-public class RNProximityModule extends ReactContextBaseJavaModule implements SensorEventListener {
+public class RNProximityModule extends ReactContextBaseJavaModule {
 
   private static final String TAG = "RNProximityModule";
-  private static final String KEY_PROXIMITY = "proximity";
-  private static final String KEY_DISTANCE = "distance";
-  private static final String KEY_EVENT_ON_SENSOR_CHANGE = "EVENT_ON_SENSOR_CHANGE";
-  private static final String EVENT_ON_SENSOR_CHANGE = "onSensorChanged";
   private final ReactApplicationContext reactContext;
 
-  private SensorManager mSensorManager;
-  private Sensor mProximity;
+  private final PowerManager.WakeLock wakeLock;
 
   public RNProximityModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
-    mSensorManager = (SensorManager) reactContext.getSystemService(Context.SENSOR_SERVICE);
-    mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    PowerManager powerManager = (PowerManager) reactContext.getSystemService(POWER_SERVICE);
+    wakeLock = powerManager.newWakeLock(PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK, TAG + ":wakelocktag");
   }
 
   public void sendEvent(String eventName, @Nullable WritableMap params) {
@@ -51,42 +38,20 @@ public class RNProximityModule extends ReactContextBaseJavaModule implements Sen
 
   @ReactMethod
   public void addListener() {
-    mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    if(!wakeLock.isHeld()) {
+      wakeLock.acquire(10*60*1000L /*10 minutes*/);
+    }
   }
 
   @ReactMethod
   public void removeListener() {
-    mSensorManager.unregisterListener(this);
+    if(wakeLock.isHeld()) {
+      wakeLock.release();
+    }
   }
 
   @Override
   public String getName() {
     return "RNProximity";
-  }
-
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    constants.put(KEY_EVENT_ON_SENSOR_CHANGE, EVENT_ON_SENSOR_CHANGE);
-    return constants;
-  }
-
-  @Override
-  public void onSensorChanged(SensorEvent sensorEvent) {
-    WritableMap params = Arguments.createMap();
-
-    double distance = sensorEvent.values[0];
-    double maximumRange = mProximity.getMaximumRange();
-    boolean isNearDevice = distance < maximumRange;
-
-    params.putBoolean(KEY_PROXIMITY, isNearDevice);
-    params.putDouble(KEY_DISTANCE, distance);
-
-    sendEvent(EVENT_ON_SENSOR_CHANGE, params);
-  }
-
-  @Override
-  public void onAccuracyChanged(Sensor sensor, int i) {
-
   }
 }
